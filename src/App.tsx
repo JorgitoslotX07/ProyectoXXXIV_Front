e096d2951b1018f80534bea5057f527759472829
Daniel Sanchez Aranega <sanchez126e@gmail.com>
2025-05-13 17:01:41 +0200

    refactor: Se cambia nombre directorios y ficheros

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistroPage from "./pages/RegisterPage/RegisterPage";
+import HomePage from "./pages/HomePage/HomePage;
 
 <Route path="/registro" element={<RegistroPage />} />
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
+         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegistroPage />} />
         {/* otras rutas */}
       </Routes>
