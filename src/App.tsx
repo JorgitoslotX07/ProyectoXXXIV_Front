c7fdca2ed24e794e52e6f20978cf7bd7ae8050b6
Toni Jorda <97570939+JorgitoslotX07@users.noreply.github.com>
2025-05-13 17:23:01 +0200

    fix conflictos 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegistroPage from "./pages/RegisterPage/RegisterPage";
 import { HomePage } from './pages/HomePage/HomePage';
+import { NavbarComponent } from './components/NavbarComponent/NavbarComponent';
 
-<Route path="/registro" element={<RegistroPage />} />
+{/* <Route path="/registro" element={<RegistroPage />} /> */}
 
 function App() {
   return (
-    <Router>
+    <>
+      <NavbarComponent />
+
+
       <Routes>
-         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegistroPage />} />
-        {/* otras rutas */}
+          <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegistroPage />} />
+            {/* otras rutas */}
       </Routes>
-    </Router>
+    </>
   );
 }
 
