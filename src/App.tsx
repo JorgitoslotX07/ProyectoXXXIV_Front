685f7e890fc59e5a421f5a4aaea4483e8364b817
Daniel Sanchez Aranega <sanchez126e@gmail.com>
2025-05-13 16:53:06 +0200

    feat: Creo componente y página de formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegistroPage from "./pages/RegisterPage/RegisterPage";
+
+<Route path="/registro" element={<RegistroPage />} />
+
 function App() {
-
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegistroPage />} />
+        {/* otras rutas */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
