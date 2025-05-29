ba38809f7919f4212a20f64f54a9c74a3ce68bec
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-05-29 11:08:18 +0200

    style: Se aplica nuevo diseño de color a las tarjetas de SubCat de Coches

--- src/components/SubCategoriasComponent/SubCategoriasComponent.tsx
+++ src/components/SubCategoriasComponent/SubCategoriasComponent.tsx
@@ -27,7 +27,7 @@ export const SubCategoriasComponent: FC = () => {
 
   return (
     <>
-      <div className="px-10 py-12 text-white"> {/*bg-[#111827]  rounded-lg shadow-md*/}
+      <div className="px-10 py-12 text-white" > {/*bg-[#111827]  rounded-lg shadow-md*/}
         <h2 className="text-3xl font-bold mb-6 text-[#C4B5FD] text-center">
           Categorías | Tipo de Coche
         </h2>
@@ -36,7 +36,7 @@ export const SubCategoriasComponent: FC = () => {
           {noticiasMock.map((item) => (
             <div
               key={item.id}
-              className="w-60 flex-shrink-0 rounded-lg p-4 shadow-lg text-center"
+              className="bg-[#162947] [background-image:radial-gradient(at_47%_33%,hsl(163.5,83%,24%)_0,#0a0a0a_59%),radial-gradient(at_82%_65%,hsl(218.82,77%,21%)_0,transparent_55%)] w-60 flex-shrink-0 rounded-lg p-4 shadow-lg text-center"
             > {/*bg-[#1F2937]*/}
               <img
                 src={item.imagen}
