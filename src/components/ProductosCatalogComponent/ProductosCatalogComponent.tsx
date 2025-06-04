0b83d6fc891dbe1d87d37a88d6699262eea8b0c4
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-06-05 01:54:17 +0200

    feat: Se implementa más traducciones

--- src/components/ProductosCatalogComponent/ProductosCatalogComponent.tsx
+++ src/components/ProductosCatalogComponent/ProductosCatalogComponent.tsx
@@ -1,49 +1,32 @@
 import { type FC } from "react";
+import { useTranslation } from "react-i18next";
 import { CardcocheComponent } from "../CardProductComponent/CardProductComponent";
 import type { ProductosCatalogProps } from "../../interfaces/ProductosCatalogProps";
 
 export const ProductosCatalogComponent: FC<ProductosCatalogProps> = ({
   vehiculos,
+  modoClaro,
 }) => {
+  const { t } = useTranslation();
+
   return (
-    // <>
-    //   <div className="flex justify-between items-center mb-2">
-    //     <p className="text-sm text-gray-800 font-medium">
-    //       {vehiculos.totalElements} Products
-    //     </p>
-    //     <p className="text-sm text-gray-700 hover:underline cursor-pointer">
-    //       Cambiar vista
-    //     </p>
-    //   </div>
-
-    //   <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-    //     {vehiculos.content.map((product, index) => (
-    //       // product.estado == "DISPONIBLE" && (
-    //       <Link to="/catalog/carDetail" state={product} key={index}>
-    //         <CardcocheComponent
-    //           key={index}
-    //           coche={product}
-    //           index={index}
-    //         />
-    //       </Link>
-    //       // )
-    //     ))}
-    //   </div>
-    // </>
-
     <>
       <div className="flex justify-between items-center mb-4 px-4">
-        <p className="text-sm text-gray-300 font-semibold">
-          {vehiculos.totalElements} Products
+        <p className={`text-sm font-semibold ${modoClaro ? "text-gray-700" : "text-gray-300"}`}>
+          {vehiculos.totalElements} {t("catalog.products")}
         </p>
-        <p className="text-sm text-gray-400 hover:text-white hover:underline cursor-pointer transition">
-          Cambiar vista
+        <p
+          className={`text-sm cursor-pointer transition hover:underline ${
+            modoClaro ? "text-gray-500 hover:text-black" : "text-gray-400 hover:text-white"
+          }`}
+        >
+          {t("catalog.changeView")}
         </p>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 px-4">
         {vehiculos.content.map((product, index) => (
-          <CardcocheComponent coche={product} index={index}/>
+          <CardcocheComponent key={index} coche={product} index={index} />
         ))}
       </div>
     </>
